07a5b5d87c38d7993c8110a8caa3b98e8e7fca5b
dohoon0508 <dohoon0508@gmail.com>
2025-03-25 23:48:15 +0900

    [Silver IV] Title: 동전 0, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,10 @@
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+count = 0
+for coin in reversed(coins):
+    if k >= coin:
+        count += k // coin
+        k %= coin
+
+print(count)
